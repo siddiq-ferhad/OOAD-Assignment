22897a9d252e299f68fd0e1b2e1a1d64102516c9
Siddiq Ferhad <123736322+siddiq-ferhad@users.noreply.github.com>
2024-01-29 18:27:57 +0800

    Update Plus.java

--- TalabiaChess v4/Plus.java
+++ TalabiaChess v4/Plus.java
@@ -5,6 +5,7 @@ public class Plus extends Piece {
     }
 
     @Override
+    // Check if the movement is valid based on piece's logic
     public boolean isValidMove(int fromRow, int fromCol, int toRow, int toCol) {
         // Move either vertically or horizontally, but not diagonally
         return fromRow == toRow || fromCol == toCol;
